329bab26a6a3585059e18ab4c948b5d2d64d68fe
Deepak Sharma <sharma.deepak8616@gmail.com>
2022-05-06 13:56:25 -0400

    Code test2 questions

---
+++ src/test/java/Class7/CodeTest2.java
@@ -0,0 +1,86 @@
+package Class7;
+
+import java.util.Arrays;
+
+public class CodeTest2 {
+    /**
+     * Ques-1:
+     * Create a method to return a max value from an input-int-array
+     * points: 10
+     */
+    /*
+     *
+     * eg:
+     * input -> [32, 54, 12, 67, 2, 5]
+     * returned value ->
+     *
+     * input -> [5, 4, 3, 1, 2]
+     * returned value ->
+     *
+     * input -> [90, 34, 12, 4]
+     * returned value ->
+     *
+     * input -> [-15, -4, -12, -7]
+     * returned value ->
+     */
+
+    /**
+     * Ques-2:
+     * Create a method to return factorial value of input-int-value
+     * points: 20
+     */
+    /*
+     *
+     * eg:
+     * input -> 5
+     * returned value -> () =
+     *
+     * input -> 1
+     * returned value -> () =
+     *
+     * input -> 3
+     * returned value -> () =
+     *
+     * input -> 10
+     * returned value -> () =
+     */
+
+    /**
+     * Ques-3:
+     * Create a method to return the common Strings values in two input-String-arrays
+     * points: 30
+     * Note: two input-String-arrays can be of same size or different size
+     */
+    /*
+     *
+     * eg:
+     * input -> {"happY", "king", "peace", "living standard"} , {"king kong", "Happy", "PEACE"}
+     * returned value -> [happy, peace]
+     *
+     * input -> {“ab”, “abcd”, “abc”, “abcde”, “defg”, "koli”} , {“abcde”, “jhuy”, “plot”, “koli”}
+     * returned value -> [abcde, koli]
+     *
+     */
+
+    /**
+     * Ques-4:
+     * Create a method to return missing smallest positive integer (greater than 0) from given int-array.
+     * points: 40
+     */
+    /*
+     *
+     * eg:
+     * 	input -> : {3, 5, 1, 4, 2, 7}
+     * 	returned value :
+     *
+     * 	input -> {2, 5, -1, 0, 6}
+     * 	returned value :
+     *
+     * 	input -> {0, 5, -1, 1, 2, 5, 3, 7, 1, 2}
+     * 	returned value :
+     *
+     * 	input -> {-2, -5, -1, -10, -6}
+     * 	returned value :
+     *
+     */
+}
